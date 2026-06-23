c1a9c9c11b6ec485908f4df0204cf07e51c5f7a5
clownoftheweekcode <clownofthe_week.code@outlook.com>
2026-06-23 18:14:29 +0000

    nothing was edited for index.html added scoring ish system (although it is not done) nothing was edited for style.css

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let diceWrap = document.querySelector('.diceRoll');
 let allDie = document.querySelectorAll('.diceRoll div');
+let allScoreSingles = document.querySelectorAll('.choiceScoreP');
 let rollNums = [0,0,0,0,0];
 let heldDie = [false,false,false,false,false];
+let answeredSingles = [false, false, false, false, false];
 let rolls = 3;
 let rollP = document.getElementById('rollsNum');
 // when you roll and you hold, if it doesnt equal 0 (placeholder), replace it
@@ -54,15 +56,46 @@ function hold(dieNum) {
         
     }
 }
-
-
+let possScores = [
+    {name: 1, value: 0, num: 0},
+    {name: 2, value: 0, num: 0},
+    {name: 3, value: 0, num: 0},
+    {name: 4, value: 0, num: 0},
+    {name: 5, value: 0, num: 0},
+    {name: 6, value: 0, num: 0},
+];
 function checkPossibleScores() {
-    if (rollNums.includes(1)) {
-        for (p = 0; p < rollNums.length; p++) {
-            if (rollNums[p] === 1) {
-                var ones = ones + 1;
-            }
+    console.clear();
+    possScores.forEach(score => {
+        score.num = rollNums.filter(num => num === score.name).length;
+        score.value = score.name * score.num;
+        // console.log(score.value);
+    });
+    for (w = 0; w < possScores.length; w++) {
+        let val = possScores[w].value;
+        if (answeredSingles[w] != true || answeredSingles[w].style.color != "black") {
+            allScoreSingles[w].textContent = val;
+            allScoreSingles[w].style.color = "blue";
+            // make it so that when you roll again it doesnt remove the value
+        } else {
+            
         }
-        console.log("ones: " + ones);
+        
     }
+    click();
+}
+function click() {
+    allScoreSingles.forEach(score => {
+        score.addEventListener('click', function(vali){
+            if (answeredSingles[score] != true) {
+                // amek it so that it goes through every element and resets the text content except for the one you clicked
+                score.style.color = "black";
+                for (e=0;e<possScores.length; e++) {
+                    if (allScoreSingles[e].style.color === "blue") {
+                        allScoreSingles[e].textContent = "";
+                    }
+                }
+            }
+        });
+    });
 }
